1346737c8ccf9a37c1dec2e6e71fa12c8e588f73
Vita <vitalia@myrambler.ru>
2019-09-25 20:19:22 +0300

    + styling

--- project1/components/user-list/user-listController.js
+++ project1/components/user-list/user-listController.js
@@ -1,12 +1,16 @@
-'use strict';
+"use strict";
 
 // cs142App.controller('UserListController', ['$scope', function ($scope) {
-cs142App.controller('UserListController', ['$scope', '$resource', function ($scope, $resource) {
-    $scope.main.title = 'Users';
+cs142App.controller("UserListController", [
+  "$scope",
+  "$resource",
+  function($scope, $resource) {
+    $scope.main.title = "Users";
     $scope.users = {};
 
-    var Users = $resource('/user/list');
+    var Users = $resource("/user/list");
     Users.query({}, function(users) {
-        $scope.users.list = users;
+      $scope.users.list = users;
     });
-}]);
+  }
+]);
